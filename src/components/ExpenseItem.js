7696423ca9e3ebb5a850099c9e95778598f18839
Praphul04 <praphulthakur6@gmail.com>
2023-07-14 09:56:27 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpanseItem() {
+    return(
+    <div>
+        <div><h1>Expanse Items</h1></div>
+        <div> Food Rs 10 </div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+    </div>
+    )
+}
+
+export default ExpanseItem;
