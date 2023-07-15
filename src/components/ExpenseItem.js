4612f69605b440b1ac960a22eaf2b1ca56f23d59
Praphul04 <praphulthakur6@gmail.com>
2023-07-15 12:38:54 +0530

    first commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,24 @@
-function ExpanseItem() {
-    return(
-    <div>
-        <div><h1>Expanse Items</h1></div>
-        <div> Food Rs 10 </div>
-        <div>Petrol Rs 100</div>
-        <div>Movies Rs 200</div>
-    </div>
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    // const expenseDate = new Date(2023, 7, 15);
+    // const expanseTitle ='Car Insurence';
+    // // const expanseAmount = 500000 ;
+    // const LocationOfExpenditure = 'Car Insurense';
+
+    return (
+        <div className='expense-item'>
+            <div>{props.date.toISOString()}</div>
+            <div className='expense-item_description'>
+                <h2>{props.title}</h2>
+                <div className='LocationOfExpenditure mx-2'>
+                    <h3>{props.LocationOfExpenditure}</h3>
+                </div>
+                <div className='expense-item_price'>Rs. {props.Amount}
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default ExpanseItem;
+export default ExpenseItem;
