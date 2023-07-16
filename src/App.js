c7661bb7f4629f7a620544cf8d3f2666f2925f44
Praphul04 <praphulthakur6@gmail.com>
2023-07-16 12:20:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import Expenses from './components/Expense';
 
-import './App.css';
-import ExpenseItem from './components/ExpenseItem'
 function App() {
-
   const expenses = [
-    { title: 'Car Insurence', Amount: 500000, date: new Date(2023, 7, 15), LocationOfExpenditure: 'CarAgency' },
-    { title: 'Toilet Paper', Amount: 500, date: new Date(2023, 6, 5), LocationOfExpenditure: 'Mall' },
-    { title: 'Shoping', Amount: 70000, date: new Date(2023, 2, 10), LocationOfExpenditure: 'Clothes' },
-    { title: 'Movie', Amount: 1000, date: new Date(2023, 5, 13), LocationOfExpenditure: 'Movie' }
-  ]
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
-    <>
     <div>
-      <ExpenseItem title={expenses[0].title} Amount={expenses[0].Amount} date={expenses[0].date} LocationOfExpenditure={expenses[0].LocationOfExpenditure}></ExpenseItem >
-      <ExpenseItem title={expenses[1].title} Amount={expenses[1].Amount} date={expenses[1].date} LocationOfExpenditure={expenses[1].LocationOfExpenditure}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} Amount={expenses[2].Amount} date={expenses[2].date} LocationOfExpenditure={expenses[2].LocationOfExpenditure}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} Amount={expenses[3].Amount} date={expenses[3].date} LocationOfExpenditure={expenses[3].LocationOfExpenditure}></ExpenseItem>
- 
-      </div>
-      
-  </>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
   );
 }
 
